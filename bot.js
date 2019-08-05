a48ba00adf232503a7021e609c76f4d1381c2cd1
venom-bd <53724912+venom-bd@users.noreply.github.com>
2019-08-05 11:25:57 +0600

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,28 @@
+var Discord = require('discord.js');
+var botName = require('./package.json');
+var bot = new Discord.Client();
+
+bot.on('message', message =>{
+
+    // Variables
+    var sender = message.author; //The person who sent the message
+    var msg = message.content;
+    var prefix = "%" //The text before commands, you can put anything that you prefer
+
+    if(message.author.id != "605714296273502211" && message.channel.id === "592736982300491776"){
+        if(msg.startsWith('%', 0)){
+            if(msg === prefix + "hey" && message.channel.id === "592736982300491776"){
+                message.channel.send('hi!!!!!') // Sends a message to the channel, with the content
+            }
+            else{
+                message.channel.send("Hey, I only understand >bang right now. More will be added when my creator is free. :) ")
+            }
+        }
+        else{
+            message.delete();
+            message.channel.send(sender + " this bot only accepts commands which starts with '%' .");
+        }
+    }
+});
+
+bot.login(process.env.BOT_TOKEN)
